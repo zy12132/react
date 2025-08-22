35313e36e87c3ba22da4cb560fad5be97f512466
zy <2407269492@qq.com>
2025-08-22 15:41:06 +0800

    'echarts'

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,18 @@
+import { BarChart } from './components/BarChart'
+
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div>
+      <BarChart
+        xData={['Vue', 'React', 'Angular']}
+        sData={[2000, 5000, 1000]} />
+
+      <BarChart
+        xData={['Vue', 'React', 'Angular']}
+        sData={[200, 500, 100]}
+        style={{ width: '500px', height: '400px' }} />
+    </div >
+  )
 }
+
 export default Home
